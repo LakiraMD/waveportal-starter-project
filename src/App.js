4e9ca48f13c6d78cb969e5d8e9315ba2244cc6ec
LeomundHunt <mdlakiramd@gmail.com>
2021-11-27 15:36:06 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import * as React from "react";
 import { ethers } from "ethers";
-import './App.css';
+import "./App.css";
 
 export default function App() {
+  const wave = () => {};
 
-  const wave = () => {
-    
-  }
-  
   return (
     <div className="mainContainer">
       <div className="dataContainer">
-        <div className="header">
-            👋Hey there!
-        </div>
+        <div className="header">👋Hey there! Welcome to Metaverse!👽</div>
 
         <div className="bio">
           I'm Lakira. I am a 15 years old developer moving to web3 from web2.0.
@@ -23,6 +18,12 @@ export default function App() {
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+        <div className="connect">
+          🎯-Follow me on Twitter for more information!🐦
+          <a href="https://twitter.com/Lakira_md" target="_blank">
+            <button className='btn'>@Lakira_md</button>
+          </a>
+        </div>
       </div>
     </div>
   );
